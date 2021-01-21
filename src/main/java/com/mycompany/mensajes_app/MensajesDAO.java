10282d67d72e3f37c797c0aad51aa09f7a377f8a
Edgar Ceron <edg-ceron@hotmail.com>
2021-01-20 18:08:57 -0600

    Se crea menú de la interfaz y se crean los layouts de los archivos a utilizar

---
+++ src/main/java/com/mycompany/mensajes_app/MensajesDAO.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mensajes_app;
+
+/**
+ *
+ * @author eceron
+ */
+public class MensajesDAO {
+    public static void CrearMensajeDB(Mensajes mensaje) {
+        
+    }
+    public static void leerMensajesDB() {
+        
+    }
+    public static void borrarMensajesDB(int id_mensaje){
+        
+    }
+    
+    public static void actualizarMensajeDB(Mensajes mensaje) {
+    }
+}

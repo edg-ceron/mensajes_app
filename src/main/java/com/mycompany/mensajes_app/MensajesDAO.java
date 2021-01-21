edc01577c28f8a2e641bdd657f515093377317c5
Edgar Ceron <edg-ceron@hotmail.com>
2021-01-20 18:41:10 -0600

    Se crea funcion para poder crear un mensaje mandando como campos el mensaje y autor e insertarlo en la BD

--- src/main/java/com/mycompany/mensajes_app/MensajesDAO.java
+++ src/main/java/com/mycompany/mensajes_app/MensajesDAO.java
@@ -5,13 +5,32 @@
  */
 package com.mycompany.mensajes_app;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author eceron
  */
 public class MensajesDAO {
     public static void CrearMensajeDB(Mensajes mensaje) {
-        
+        Conexion db_connect = new Conexion();
+        try (Connection conexion = db_connect.get_connection()){
+            PreparedStatement ps = null;
+            try {
+                String queary = "INSERT INTO mensajes (`mensaje`, `autor_mensaje`) VALUES (?,?);";
+                ps = conexion.prepareStatement(queary);
+                ps.setString(1, mensaje.getMensaje());
+                ps.setString(2, mensaje.getAutor_mensaje());
+                ps.executeUpdate();
+                System.out.println("Mensaje creado");
+            } catch (SQLException ex){
+                System.out.println(ex);
+            }
+        } catch(SQLException e) {
+            System.out.println(e);
+        }
     }
     public static void leerMensajesDB() {
         
--- src/main/java/com/mycompany/mensajes_app/MensajesService.java
+++ src/main/java/com/mycompany/mensajes_app/MensajesService.java
@@ -5,13 +5,28 @@
  */
 package com.mycompany.mensajes_app;
 
+import java.util.Scanner;
+
 /**
  *
  * @author eceron
  */
 public class MensajesService {
     public static void crearMensaje (){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe tu mensaje"); 
+        String mensaje = sc.nextLine();
         
+        System.out.println("Autor del mensaje");
+        String nombre = sc.nextLine();
+        
+        Mensajes registro = new Mensajes();
+        registro.setMensaje(mensaje);
+        registro.setAutor_mensaje(nombre);
+        
+        MensajesDAO.CrearMensajeDB(registro);
+        
+       
     }
     
     public static void listarMensajes() {
